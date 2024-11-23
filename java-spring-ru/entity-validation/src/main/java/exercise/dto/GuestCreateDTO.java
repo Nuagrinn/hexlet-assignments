c3bd8395a6354651ddfc16870bcb9796d41b7575
Владислав <megoboom17@gmail.com>
2024-11-23 19:50:27 +0300

    1.0.1

---
+++ java-spring-ru/entity-validation/src/main/java/exercise/dto/GuestCreateDTO.java
@@ -0,0 +1,34 @@
+package exercise.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class GuestCreateDTO {
+
+    @NotNull
+    private String name;
+
+    @Email
+    private String email;
+
+    @Pattern(regexp = "^\\+.*")
+    @Size(min=11, max=13)
+    private String phoneNumber;
+
+    @Pattern(regexp = "\\d{4}", message = "Поле должно содержать ровно 4 цифры")
+    private String clubCard;
+
+    @FutureOrPresent(message = "Дата должна быть в будущем или настоящем")
+    private LocalDate cardValidUntil;
+
+}
