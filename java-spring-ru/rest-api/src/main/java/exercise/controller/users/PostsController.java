cf0d1788cf585aeaec6e38115f3182014ca60bf7
Vladislav <megoboom17@gmail.com>
2025-01-30 17:55:32 +0300

    init 1.0.1

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -2,7 +2,7 @@ package exercise.controller.users;
 
 import java.util.List;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
